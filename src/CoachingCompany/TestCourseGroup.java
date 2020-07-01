fab419bf274918f05abd858f55e8f1daa13bec7e
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-07-01 20:30:34 +0200

    Small correction.

--- src/CoachingCompany/TestCourseGroup.java
+++ src/CoachingCompany/TestCourseGroup.java
@@ -5,10 +5,9 @@ public class TestCourseGroup {
         CourseGroup courseGroup = new CourseGroup();
         new Teacher();
         Teacher teacher = courseGroup.newTeacher(new Teacher("Brzozowa", 14, 32, 5555, "Kąty Wrocławskie", "Polska", "Alfred", "Alberski", 33, 1111111, 111111111, 4500.99, 299.99, 3, 1, 40));
-        CourseGroup courseGroupone = new CourseGroup(new Teacher("Brzozowa", 14, 32, 5555, "Kąty Wrocławskie", "Polska", "Alfred", "Alberski", 33, 1111111, 111111111, 4500.99, 299.99, 3, 1, 40));
         Student [] students = new Student[2];
-        students[0] = courseGroupone.enrollStudent(new Student("Toyoty", 123, 0, 14567, "Połaniewo", "Poland", "Dawid", "Fabiak", 28, 56789, 345678, 19, true, true, true, 20));
-        students[1] = courseGroupone.enrollStudent(new Student("Beskidzka", 25, 54, 98765, "Łódź", "Polska", "Marta", "Krakowska", 24, 24567, 34567, 159, false, false, false, 0));
+        students[0] = courseGroup.enrollStudent(new Student("Toyoty", 123, 0, 14567, "Połaniewo", "Poland", "Dawid", "Fabiak", 28, 56789, 345678, 19, true, true, true, 20));
+        students[1] = courseGroup.enrollStudent(new Student("Beskidzka", 25, 54, 98765, "Łódź", "Polska", "Marta", "Krakowska", 24, 24567, 34567, 159, false, false, false, 0));
 
         System.out.println(teacher);
         System.out.println(students[0].toString());
